324c5983e280a1a453d5104b76eadb2896acfb6c
woodnorth <woodnorth@self.com>
2024-04-28 13:13:34 +0800

    add ResultDict func

--- resultdict.go
+++ resultdict.go
@@ -1,10 +1,19 @@
 package chardet
 
-//type ResultDict map[string]interface{}
-//
-
 type ResultDict struct {
 	encoding   string
 	confidence float64
 	language   string
+}
+
+func (r *ResultDict) GetEncoding() string {
+	return r.encoding
+}
+
+func (r *ResultDict) GetConfidence() float64 {
+	return r.confidence
+}
+
+func (r *ResultDict) GetLanguage() string {
+	return r.language
 }
